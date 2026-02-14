8406165e29876910892159f8da9452325da0f012
anandhu <anandhuzz916@gmail.com>
2026-02-14 12:37:33 +0530

    added auth controller

---
+++ src/main/java/com/spring/tradexidentityservice/Controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.spring.tradexidentityservice.Controller;
+
+import com.spring.tradexidentityservice.DTO.AuthResponse;
+import com.spring.tradexidentityservice.DTO.LoginRequest;
+import com.spring.tradexidentityservice.DTO.RegisterRequest;
+import com.spring.tradexidentityservice.Service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody RegisterRequest request) {
+        authService.register(request);
+        return new AuthResponse("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request){
+        String token = authService.authenticate(request.getEmail(), request.getPassword());
+
+        return new AuthResponse(token);
+    }
+}
