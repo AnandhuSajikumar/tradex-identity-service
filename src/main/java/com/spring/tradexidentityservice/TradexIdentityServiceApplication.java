2763ead834a5e4febe4c18f80a4b3c41aeb24930
anandhu <anandhuzz916@gmail.com>
2026-02-18 20:34:44 +0530

    databas via docker

--- src/main/java/com/spring/tradexidentityservice/TradexIdentityServiceApplication.java
+++ src/main/java/com/spring/tradexidentityservice/TradexIdentityServiceApplication.java
@@ -3,10 +3,13 @@ package com.spring.tradexidentityservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TradexIdentityServiceApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(TradexIdentityServiceApplication.class, args);
     }
 
